8d315008c5e7e307b8dfa50a177ae74a48f45c1c
taylor-lindsay <taylor_lindsay@uri.edu>
2021-11-01 16:10:17 -0400

    updating notes & data analysis file

--- data_analysis_files/benthic_cover_analysis.R
+++ data_analysis_files/benthic_cover_analysis.R
@@ -19,7 +19,7 @@ site_classification_database3 <- read.csv('~/Desktop/GITHUB/PR_Fish/raw_data3/PR
 species_info <- read.csv('~/Desktop/GITHUB/PR_Fish/data_analysis_files/species_info.csv')
 #clean datasets
 
-# Clean data
+# Clean data labels 
 abundance_data3$DEPTH.ZONE <- abundance_data3$DEPTH.ZONE %>% 
   gsub("intermediate", "Intermediate", .) %>%
   gsub("shallow", "Shallow", .) %>%
@@ -48,7 +48,9 @@ biomass_data3$SITE.NAME <- biomass_data3$SITE.NAME %>%
   gsub("Maria Langa 5m ", "Maria Langa 5m", .) %>%
   gsub("Canal Luis Pea", "Canal Luis Peña", .) 
 
-benthic_data3$SITE.NAME <- benthic_data3$SITE.NAME %>% 
+names(site_classification_database3) <- toupper(names(site_classification_database3))
+
+site_classification_database3$SITE.NAME <- site_classification_database3$SITE.NAME %>% 
   gsub("Berbera", "Berbería", .)  %>%
   gsub("Caa Gorda", "Caña Gorda", .) %>%
   gsub("Windward Reef ", "Windward Reef", .) %>%
@@ -56,7 +58,40 @@ benthic_data3$SITE.NAME <- benthic_data3$SITE.NAME %>%
   gsub("Maria Langa 5m ", "Maria Langa 5m", .) %>%
   gsub("Canal Luis Pea", "Canal Luis Peña", .) 
 
-# 
+benthic_data3$SITE.NAME <- benthic_data3$SITE.NAME %>% 
+  gsub("BerberÕa", "Berbería", .)  %>%
+  gsub("CaÐa Gorda", "Caña Gorda", .) %>%
+  gsub("Windward Reef ", "Windward Reef", .) %>%
+  gsub("West Caballo Blanco (2001)", "West Caballo Blanco", .) %>%
+  gsub("Maria Langa 5m ", "Maria Langa 5m", .) %>%
+  gsub("Canal Luis PeÐa", "Canal Luis Peña", .) 
 
+# Save just the columns I want & merge with site info 
+benthic_totals <- benthic_data3 %>% dplyr:: select(grep("total", names(benthic_data3)),
+      grep("YEAR", names(benthic_data3)),
+      grep("LOCATION", names(benthic_data3)),
+      grep("SITE.NAME", names(benthic_data3))) %>%
+      full_join(., site_classification_database3, 
+            by = "SITE.NAME")
+    
+# Graph average coral cover by year 
+
+benthic_totals_means <- benthic_totals %>% filter(YEAR)
+
+
+
+
+
+
+
+# filter down to just Cabo Rojo
+# benthic_cabo_rojo <- benthic_totals %>% filter(LOCATION == "Cabo Rojo")
+
+# add MPA data column
+#benthic_cabo_rojo$MPA <- 
+#if (benthic_cabo_rojo$SITE.NAME == "Resuellos") {benthic_cabo_rojo$MPA = "Yes"} 
+  #else {benthic_cabo_rojo$MPA = "no"} benthic_cabo_rojo <- benthic_cabo_rojo %>%
+  #mutate(MPA = case_when(
+  #  benthic_cabo_rojo$SITE.NAME == "Resuellos" ~ "Recovered" ))
 
 
